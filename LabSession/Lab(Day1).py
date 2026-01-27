5ab84a14ef31124b25eebeb45a185f7f7374b8ea
Your Name <CristianSR344>
2026-01-27 16:41:16 -0600

    commit

--- LabSession/Lab(Day1).py
+++ LabSession/Lab(Day1).py
@@ -1,16 +1,31 @@
-first_name = input("Enter First name: ")
-paternal_name = input("Enter Paternal name: ")
-maternal_name = input("Enter Maternal name: ")
-# dob = input("Enter your DoB \"YYYY-MM-DD\": ")
-# sex = input("\"H\" or \"M\": ")
-# state = input("DF or BC: ")
-
-curp = first_name[:1].upper() + paternal_name[:1].upper() + maternal_name[:1].upper()
-print(curp)
-
-first_name
-paternal_last
-maternal_last
-dob
-sex
-state
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "5053f3f1",
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "first_name = input(\"Please enter your first name\")\n"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "8f41e67a",
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "first_name = input(\"Please enter your first name\")\n"
+   ]
+  }
+ ],
+ "metadata": {
+  "language_info": {
+   "name": "python"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
